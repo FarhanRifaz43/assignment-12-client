9d16b67cddcd2ea57d4ef8f874629a4bfaa3e721
Farhan Rifaz <farhanrifaz@Farhans-MacBook-Pro.local>
2023-11-25 21:41:38 +0600

    added overview

--- src/components/pages/home/Tourism/TabOne.jsx
+++ src/components/pages/home/Tourism/TabOne.jsx
@@ -1,7 +1,42 @@
 const TabOne = () => {
     return (
-        <div>
-            sdfsdfsdfs
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 w-fit mx-auto gap-8 mt-10">
+            <div className="w-96 bg-base-200">
+                <div className="my-8 px-4">
+                    <h2 className="text-left font-bold text-2xl mb-3">Guided Adventures Await</h2>
+                    <p className="text-left text-gray-600"> Embark on unforgettable journeys with our expert guides. From historical landmarks to cultural hotspots, our guided tours offer a unique blend of education and adventure. Immerse yourself in diverse experiences, connect with local communities, and leave with a deeper understanding of the world around you. Your next adventure starts here.</p>
+                </div>
+            </div>
+            <div className="w-96 bg-base-200">
+                <div className="my-8 px-4">
+                    <h2 className="text-left font-bold text-2xl mb-3">Explore Diversity</h2>
+                    <p className="text-left text-gray-600">Dive into the heart of cultures worldwide with our Cultural Expeditions. Whether you're a seasoned traveler or a first-time explorer, our tours celebrate the richness of global diversity. Engage in local traditions, savor authentic cuisines, and witness the beauty of different lifestyles. Join us for an enriching journey that transcends boundaries and fosters a deeper appreciation for the world's cultures.</p>
+                </div>
+            </div>
+            <div className="w-96 bg-base-200">
+                <div className="my-8 px-4">
+                    <h2 className="text-left font-bold text-2xl mb-3">Aesthetic Discoveries</h2>
+                    <p className="text-left text-gray-600">Immerse yourself in the world of art and architecture with our curated experiences. From prestigious galleries to iconic landmarks, our tours unveil the stories behind the masterpieces. Whether you're an art aficionado or a casual observer, let us guide you through a visual feast that transcends time and style. Unearth the aesthetic wonders that shape our cultural landscape.</p>
+                </div>
+            </div>
+            <div className="w-96 bg-base-200">
+                <div className="my-8 px-4">
+                    <h2 className="text-left font-bold text-2xl mb-3"> Eco-Exploration</h2>
+                    <p className="text-left text-gray-600">Escape the ordinary and reconnect with nature through our Eco-Exploration tours. Venture into diverse ecosystems, from lush forests to serene meadows, with knowledgeable guides leading the way. Learn about the delicate balance of our environment, engage in conservation efforts, and relish the serenity of unspoiled landscapes. Nature's wonders await your discovery.</p>
+                </div>
+            </div>
+            <div className="w-96 bg-base-200">
+                <div className="my-8 px-4">
+                    <h2 className="text-left font-bold text-2xl mb-3">Urban Discovery</h2>
+                    <p className="text-left text-gray-600">Uncover the secrets of cityscapes and skylines with our Urban Discovery tours. Traverse bustling streets, step into architectural marvels, and absorb the energy of vibrant urban life. Led by passionate guides, these tours offer a blend of historical insights and modern marvels. Immerse yourself in the dynamic pulse of cities around the globe.</p>
+                </div>
+            </div>
+            <div className="w-96 bg-base-200">
+                <div className="my-8 px-4">
+                    <h2 className="text-left font-bold text-2xl mb-3">Gastronomic Adventures</h2>
+                    <p className="text-left text-gray-600">Embark on a flavorful odyssey with our Culinary Journeys, where every dish tells a story. Join us to explore the world through its diverse cuisines, from street food gems to fine dining experiences. Our expert guides will lead you through markets, kitchens, and dining tables, offering insights into the cultural significance of each dish. Savor the tastes, aromas, and traditions that make every meal a journey in itself. Let your taste buds travel with us.</p>
+                </div>
+            </div>
         </div>
     );
 };
